76aaf526a744dd5ed09db994a0dd374674caf25f
Jeffrey Wang <wangjeffrey30@gmail.com>
2023-08-30 14:43:49 -0400

    refactored home into navbar + headline + rest

--- app/page.tsx
+++ app/page.tsx
@@ -1,105 +1,14 @@
+import Navbar from './components/HomePage/Navbar'
+import Headline from './components/HomePage/Headline'
+
 export default function Home() {
 return (
   <>
   <main className="gradient leading-relaxed tracking-wide flex flex-col">
-    <nav id="header" className="w-full z-30 top-0 text-white py-1 lg:py-6">
-      <div
-        className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-2 lg:py-6"
-      >
-        <div className="pl-4 flex items-center">
-          <a
-            className="text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
-            href="#"
-          >
-            <svg
-              className="h-6 w-6 inline-block fill-current text-yellow-700"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <path d="M13 8V0L8.11 5.87 3 12h4v8L17 8h-4z" />
-            </svg>
-            ospo
-          </a>
-        </div>
-
-        <div className="block lg:hidden pr-4">
-          <button
-            id="nav-toggle"
-            className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-green-500 appearance-none focus:outline-none"
-          >
-            <svg
-              className="fill-current h-3 w-3"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
-
-        <div
-          className="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden lg:block mt-2 lg:mt-0 text-black p-4 lg:p-0 z-20"
-          id="nav-content"
-        >
-          <ul className="list-reset lg:flex justify-end flex-1 items-center">
-            <li className="mr-3">
-              <a
-                className="inline-block py-2 px-4 text-black font-bold no-underline"
-                href="#"
-                >Active</a
-              >
-            </li>
-            <li className="mr-3">
-              <a
-                className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                href="#"
-                >link</a
-              >
-            </li>
-            <li className="mr-3">
-              <a
-                className="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-                href="#"
-                >link</a
-              >
-            </li>
-          </ul>
-          <button
-            id="navAction"
-            className="mx-auto lg:mx-0 hover:underline text-gray-800 font-extrabold rounded mt-4 lg:mt-0 py-4 px-8 shadow opacity-75"
-          >
-            Action
-          </button>
-        </div>
-      </div>
-    </nav>
+    <Navbar />
 
   <div className="container mx-auto h-screen">
-    <div className="text-center px-3 lg:px-0">
-      <h1
-        className="my-4 text-2xl md:text-3xl lg:text-5xl font-black leading-tight"
-      >
-        Tired of Tracking Job Applications in Spreadsheets?
-      </h1>
-      <p
-        className="leading-normal text-gray-800 text-base md:text-xl lg:text-2xl mb-8"
-      >
-        Sub-hero message, not too long and not too short. Make it just right!
-      </p>
-
-      <button
-        className="mx-auto lg:mx-0 hover:underline text-gray-800 font-extrabold rounded my-2 md:my-6 py-4 px-8 shadow-lg w-48"
-      >
-        Sign Up
-      </button>
-      <a
-        href="#"
-        className="inline-block mx-auto lg:mx-0 hover:underline bg-transparent text-gray-600 font-extrabold my-2 md:my-6 py-2 lg:py-4 px-8"
-        >View Additional Action</a
-      >
-    </div>
-
+    <Headline />
     <div className="flex items-center w-full mx-auto content-end">
       <div
         className="relative browser-mockup flex flex-1 m-6 md:px-0 md:m-12 bg-white w-1/2 rounded shadow-xl"
@@ -167,8 +76,8 @@ return (
               d="M119.9,721.42c-3-5.51.4-12.27,4.29-17.18s8.61-10,8.51-16.29c-.15-9-9.7-14.31-17.33-19.09a84,84,0,0,1-15.56-12.51A22.8,22.8,0,0,1,95,650c-1.58-3.52-1.54-7.52-1.44-11.37q.51-19.26,1.91-38.49"
               fill="none"
               stroke="#3f3d56"
-              stroke-miterlimit="10"
-              stroke-width="4"
+              strokeMiterlimit="10"
+              strokeWidth="4"
             />
             <path
               transform="translate(-11.5 -150.75)"
@@ -245,49 +154,49 @@ return (
               d="M387.5,490A66.5,66.5,0,1,1,321,423.5,66.47,66.47,0,0,1,387.5,490Z"
               fill="none"
               stroke="#f2f2f2"
-              stroke-miterlimit="10"
-              stroke-width="2"
+              strokeMiterlimit="10"
+              strokeWidth="2"
             />
             <path
               transform="translate(-11.5 -150.75)"
               d="M325.38,467.23l8.3,13,35.53,55.59a66.5,66.5,0,0,1-103.32-8.57l43.54-84.94.91,1.43"
               fill="none"
               stroke="#f2f2f2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
             />
             <path
               transform="translate(-11.5 -150.75)"
               d="M385.31,507a66.46,66.46,0,0,1-16.1,28.82l-35.53-55.59,15.69-24.78a.66.66,0,0,1,1.1,0C353.76,460.32,371,486,385.31,507Z"
               fill="none"
               stroke="#f2f2f2"
-              stroke-miterlimit="10"
-              stroke-width="2"
+              strokeMiterlimit="10"
+              strokeWidth="2"
             />
             <path
               transform="translate(-11.5 -150.75)"
               d="M337.5,452.5a15,15,0,0,1-12.12,14.73l-15-23.51a15,15,0,0,1,27.16,8.78Z"
               fill="none"
               stroke="#f2f2f2"
-              stroke-miterlimit="10"
-              stroke-width="2"
+              strokeMiterlimit="10"
+              strokeWidth="2"
             />
             <path
               transform="translate(-11.5 -150.75)"
               d="m347.5 481.5"
               fill="none"
               stroke="#f2f2f2"
-              stroke-miterlimit="10"
-              stroke-width="2"
+              strokeMiterlimit="10"
+              strokeWidth="2"
             />
             <path
               transform="translate(-11.5 -150.75)"
               d="m333.5 480.5"
               fill="none"
               stroke="#f2f2f2"
-              stroke-miterlimit="10"
-              stroke-width="2"
+              strokeMiterlimit="10"
+              strokeWidth="2"
             />
             <path
               transform="translate(-11.5 -150.75)"
@@ -557,8 +466,8 @@ return (
               fill="none"
               opacity=".8"
               stroke="#ff6347"
-              stroke-miterlimit="10"
-              stroke-width="2"
+              strokeMiterlimit="10"
+              strokeWidth="2"
             />
             <path
               transform="translate(-11.697 -13.011)"
@@ -566,8 +475,8 @@ return (
               fill="none"
               opacity=".8"
               stroke="#ff6347"
-              stroke-miterlimit="10"
-              stroke-width="2"
+              strokeMiterlimit="10"
+              strokeWidth="2"
             />
             <circle cx="214.07" cy="310.65" r="11.968" fill="#ff6347" />
             <circle cx="322.2" cy="566.52" r="11.968" fill="#ff6347" />
