27089a2966df8a5f89e6e78e7e5991233bafd56b
Jeffrey Wang <wangjeffrey30@gmail.com>
2023-08-31 01:20:52 -0400

    MockBrowser - recent app section modularized

---
+++ app/components/DashBoard/RecentApplication.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+
+type Props = {
+    img: string;
+    alt: string;
+    company: string;
+    role: string;
+    timeSince: string;
+}
+
+export default function RecentApplication(props: Props) {
+    const { img, alt, company, role, timeSince } = props
+    return (
+        <li className="ml-2 pl-3 pb-3 sm:pb-4 bg-gray-100 mb-2 rounded-lg p-2">
+            <div className="flex items-center space-x-4">
+            <div className="flex-shrink-0">
+            <Image
+                src={img}
+                width={25}
+                height={25}
+                alt={alt}
+                />
+            </div>
+                <div className="flex-1 min-w-0">
+                    <p className="text-sm font-medium text-gray-900 truncate">
+                        {company}
+                    </p>
+                    <p className="text-sm text-gray-500 truncate">
+                        {role}
+                    </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                    {timeSince}
+                </div>
+            </div>
+        </li>
+    )
+}
--- app/components/HomePage/MockBrowser.tsx
+++ app/components/HomePage/MockBrowser.tsx
@@ -1,16 +1,20 @@
 import { IoInformationCircleOutline } from "react-icons/io5"
+import RecentApplication from '../Dashboard/RecentApplication'
+import AmazonPng from '../../assets/images/Amazon_icon.png'
+import MicrosoftPng from '../../assets/images/Microsoft_icon.png'
+import TeslaPng from '../../assets/images/Tesla_icon.png'
 
 export default function MockBrowser() {
     return (
         <div className="flex items-center w-full mx-auto content-end">
             <div
-                className="relative browser-mockup flex flex-1 m-6 md:px-0 md:m-12 bg-white w-1/2 rounded shadow-xl"
+                className="relative browser-mockup flex flex-1 m-6 md:px-0 md:m-12 bg-gray-200 w-1/2 rounded shadow-xl"
             >
-                <div className="flex bg-purple-700 absolute bottom-4 left-4 rounded-md pl-1 pr-1 text-white">
+                <div className="flex bg-purple-700 absolute bottom-5 left-5 rounded-md pl-1 pr-1 text-white">
                     <p className="flex items-center m-0 p-1"> <IoInformationCircleOutline className="mr-1" size={24}/> Displaying sample data</p>
                 </div>
                 <p className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-7"> My Demo Account </p>
-                    <div className="flex flex-1 flex-col bg-gray-300 w-6/12 box-border rounded-lg p-5 m-3">
+                    <div className="flex flex-1 flex-col bg-white w-6/12 box-border rounded-lg p-5 m-3 mr-0">
                         <p className="p-5 text-4xl font-bold">Your Year/Month/Week in Review </p>
                         <div className="flex items-center">
                             <p className="ml-5 text-3xl">400</p>
@@ -30,33 +34,16 @@ export default function MockBrowser() {
                         </div>
                     </div>
                     <div className="flex flex-1 flex-col">
-                        <div className="bg-blue-300 box-border rounded-lg p-5 m-3">
+                        <div className="bg-white box-border rounded-lg p-5 m-3">
                             something
                         </div>
-                        <div className="bg-green-300 box-border rounded-lg m-3">
+                        <div className="bg-white box-border rounded-lg m-3">
                             <p className="pt-5 pl-5 text-2xl font-bold">Your Recent Applications</p>
-                            <ul className="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
-                                <li className="ml-2 pl-3 pb-3 sm:pb-4 bg-gray-100 rounded-lg p-2">
-                                    <div className="flex items-center space-x-4">
-                                        <div className="flex-1 min-w-0">
-                                            <p className="text-sm font-medium text-gray-900 truncate">
-                                            Amazon
-                                            </p>
-                                            <p className="text-sm text-gray-500 truncate">
-                                            Software Engineering Intern
-                                            </p>
-                                        </div>
-                                        <div className="inline-flex items-center text-base font-semibold text-gray-900">
-                                            5 min ago
-                                        </div>
-                                    </div>
-                                </li>
+                            <ul className="max-w-md">
+                                <RecentApplication img={AmazonPng} alt={"Amazon Logo"} company={"Amazon"} role={"Software Engineer Intern"} timeSince={"5 min ago"} />
+                                <RecentApplication img={MicrosoftPng} alt={"Microsoft Logo"} company={"Microsoft"} role={"Software Engineer Intern"} timeSince={"27 min ago"} />
+                                <RecentApplication img={TeslaPng} alt={"Tesla Logo"} company={"Tesla"} role={"Data Analyst Intern"} timeSince={"2 days ago"} />
                             </ul>
-                            <ul>
-                                <li className="pl-5 p-2">Microsoft, Software Engineering Intern</li>
-                                <li className="pl-5 p-2">Tesla, Software Engineering Intern</li>
-                            </ul>
-
                         </div>
                     </div>
             </div>
