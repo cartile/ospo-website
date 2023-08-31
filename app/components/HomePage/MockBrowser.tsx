15307837a150c2a98cdc3c2db186dea18437f561
Jeffrey Wang <wangjeffrey30@gmail.com>
2023-08-31 00:40:16 -0400

    testing new recent application

--- app/components/HomePage/MockBrowser.tsx
+++ app/components/HomePage/MockBrowser.tsx
@@ -35,8 +35,24 @@ export default function MockBrowser() {
                         </div>
                         <div className="bg-green-300 box-border rounded-lg m-3">
                             <p className="pt-5 pl-5 text-2xl font-bold">Your Recent Applications</p>
+                            <ul className="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
+                                <li className="ml-2 pl-3 pb-3 sm:pb-4 bg-gray-100 rounded-lg p-2">
+                                    <div className="flex items-center space-x-4">
+                                        <div className="flex-1 min-w-0">
+                                            <p className="text-sm font-medium text-gray-900 truncate">
+                                            Amazon
+                                            </p>
+                                            <p className="text-sm text-gray-500 truncate">
+                                            Software Engineering Intern
+                                            </p>
+                                        </div>
+                                        <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                                            5 min ago
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
                             <ul>
-                                <li className="pl-5 p-2">Amazon, Software Engineering Intern</li>
                                 <li className="pl-5 p-2">Microsoft, Software Engineering Intern</li>
                                 <li className="pl-5 p-2">Tesla, Software Engineering Intern</li>
                             </ul>
